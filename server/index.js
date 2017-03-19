f5c5005d516ecf3bae5b294c8b86aa4b7b350f17
Ben Hanna <benpaulhanna@gmail.com>
2017-03-19 03:50:38 +0000

    Added missing packages and moved public folder.

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
 var app = require('koa')(),
     cors = require('kcors'),
-    bodyParser = require('koa-bodyparser');
+    parser = require('koa-bodyparser'),
+    serve = require('koa-static');
 
 app.context.knex = require('./lib/knex');
-app.context.create = function* () {
-    
-}
 
 app
     .use(cors())
-    .use(bodyParser())
-    .use(require('./v1'));
-
-app.listen(8080, start);
+    .use(parser())
+    .use(serve(__dirname + '/public'))
+    .use(require('./v1'))
+    .listen(8080, start);
 
 function start() {
     console.log('API ready');
